13e48d89e7247487fe08ef3253bb08463addaa4f
guillermoleonq <guillermoleonq@yahoo.com>
2024-11-11 20:07:59 -0500

    Configurating files & components frontend

---
+++ src/helpers/Global.jsx
@@ -0,0 +1,11 @@
+// Ruta del Backend desplegado
+  export const Global = {
+    url: "https://backend-social-net-1tng.onrender.com/api/"
+  }
+
+
+// Ruta del Backend en Local: se debe ejecutar el servidor del backend
+
+// export const Global = {
+//     url: "http://localhost:3900/api/"
+//   }
